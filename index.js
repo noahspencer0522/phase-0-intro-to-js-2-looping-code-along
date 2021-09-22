47b236d13d413b22ad49dd2985c992689bcbd326
Noah Spencer <noahspencer0522@gmail.com>
2021-09-22 13:57:19 -0500

    commit tree 5cf4968df51c33fb9259c52e77033bf858388d0f

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-let thankYou = []
-function writeCards(thankYou){
-    for (let i = 0; i < thankYou.length; i++) {
-    console.log(`Thank you, ${thankYou[i]} for the wonderful birthday gift!`)
-    debugger
-}
-
-}
-
-
+let wtiteCards = ['Lisa', 'Kaitlin', 'Jan']
 
 
 
