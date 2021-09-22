5c75f2614e33344a63e9374b2900e165ec34d60a
Noah Spencer <noahspencer0522@gmail.com>
2021-09-22 13:38:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const thankYou = ["Lisa", "Kaitlin", "Jan"];
-
-function writeCards(thankYou) {
-  for (let i = 0; i < thankYou.length; i++) {
-    console.log(`Thank you, ${thankYou[i]} for the wonderful birthday gift!`);
-    debugger;
-  }
-
-  return thankYou
+let thankYou = []
+function writeCards(thankYou){
+    for (let i = 0; i < thankYou.length; i++) {
+    console.log(`Thank you, ${thankYou[i]} for the wonderful birthday gift!`)
+    debugger
 }
 
-writeCards(thankYou);
+}
+
+
+
+
+
+
